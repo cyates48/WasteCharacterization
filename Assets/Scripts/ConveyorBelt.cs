8d73e42009a218cdd80eb94df8d83cb654bb615a
cyates48 <cmyates48@gmail.com>
2019-01-27 13:28:29 -0800

    upgraded scene, working on conveyor script

---
+++ Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour {
+
+    float speed = 15f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void onCollisionStay(Collision collision) {
+        float beltVelocity = speed * Time.deltaTime;
+        collision.gameObject.GetComponent<Rigidbody>().velocity = beltVelocity * transform.forward;
+    }
+}
