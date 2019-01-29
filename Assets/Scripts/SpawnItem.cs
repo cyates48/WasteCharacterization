8cedd0e1b9c8ecfa214ad79f0feb769631a40bf8
cyates48 <cmyates48@gmail.com>
2019-01-29 10:45:22 -0800

    updated spawn script

--- Assets/Scripts/SpawnItem.cs
+++ Assets/Scripts/SpawnItem.cs
@@ -6,14 +6,15 @@ using Random = System.Random;
 public class SpawnItem : MonoBehaviour {
 
     public GameObject fork, glass_bottle;
-    public GameObject[] models;
-
+    public GameObject[] models = new GameObject[2];
     public GameObject selector;
     Random lane_rnd = new Random();
     Random pref_rnd = new Random();
+
 	// Use this for initialization
 	void Start () {
-        models = { fork, glass_bottle };
+        models[0] = fork;
+        models[1] = glass_bottle;
         StartCoroutine(spawnItems());
 	}
 	
@@ -25,15 +26,15 @@ public class SpawnItem : MonoBehaviour {
     IEnumerator spawnItems() {
         while (true) {
             int lane = lane_rnd.Next(1, 3);
-            selector = models[pref_rnd.Next(1, 2) - 1];
-            //Debug.Log(models[0]);
+            selector = models[pref_rnd.Next(0, models.Length-1)];
+            Debug.Log(pref_rnd.Next(0, models.Length - 1));
 
             if (lane == 1)
-                Instantiate(fork, new Vector3(-2.16f, 2.348f, -1.338f), Quaternion.identity);
+                Instantiate(selector, new Vector3(-2.16f, 2.348f, -1.338f), Quaternion.identity);
             else if (lane == 2)
-                Instantiate(fork, new Vector3(-2.16f, 2.348f, -1.047f), Quaternion.identity);
+                Instantiate(selector, new Vector3(-2.16f, 2.348f, -1.047f), Quaternion.identity);
             else
-                Instantiate(fork, new Vector3(-2.16f, 2.348f, -0.78f), Quaternion.identity);
+                Instantiate(selector, new Vector3(-2.16f, 2.348f, -0.78f), Quaternion.identity);
             yield return new WaitForSeconds(2f);
         }
     }
