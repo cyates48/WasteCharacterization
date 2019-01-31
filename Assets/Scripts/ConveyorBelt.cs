eff8fbf47d55c96dda5c8f562c621d4462444aed
TKingVS <tking@scu.edu>
2019-01-31 10:46:25 -0800

    Improved conveyor belt

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class ConveyorBelt : MonoBehaviour {
 
     public float speed = 1f;
+    public GameObject spawner;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
-	void FixedUpdate () {
-        Rigidbody rigidbody = GetComponent<Rigidbody>();
-        rigidbody.position += (transform.right * Time.deltaTime * 0.4f);
-        rigidbody.MovePosition(rigidbody.position - (transform.right * Time.deltaTime *0.4f));
-    }
+	void OnCollisionStay(Collision col) {
+		Rigidbody trash_rb = col.gameObject.GetComponent<Rigidbody>();
+		Vector3[] lane_locs = spawner.GetComponent<Spawner>().lane_locations;
+		trash_rb.velocity = new Vector3(speed, 0f, 0f);
+
+		int closestlane = -1;
+		float closestdist = 999999;
+		for (int i = 0; i < lane_locs.Length; i++) {
+			float dist = Mathf.Pow(trash_rb.position.z - lane_locs[i].z, 2);
+			if (dist < closestdist) {
+				closestdist = dist;
+				closestlane = i;
+			}
+		}
+
+		trash_rb.position = new Vector3(trash_rb.position.x, trash_rb.position.y, lane_locs[closestlane].z);
+	}
 }
