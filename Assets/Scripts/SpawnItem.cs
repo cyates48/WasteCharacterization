d50f6f13909685e85cc55030d32bf7cb1b90e9e0
TKingVS <tking@scu.edu>
2019-01-31 08:57:04 -0800

    Simplified spawnitem script, now spawns multiple types of trash, modified some trash prefabs

--- Assets/Scripts/SpawnItem.cs
+++ Assets/Scripts/SpawnItem.cs
@@ -11,26 +11,14 @@ public class SpawnItem : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+    	rng = new Random();
         StartCoroutine(spawnItems());
-    }
-    
-    // Update is called once per frame
-    void Update () {
     }
 
     IEnumerator spawnItems() {
         while (true) {
             Vector3 lane_location = lane_locations[rng.Next(lane_locations.Length)];
             GameObject item = trash_items[rng.Next(trash_items.Length)];
-            //Debug.Log();
-
-            // if (lane == 0)
-            //     Instantiate(selector, new Vector3(-2.16f, 2.348f, -1.338f), Quaternion.identity);
-            // else if (lane == 1)
-            //     Instantiate(selector, new Vector3(-2.16f, 2.348f, -1.047f), Quaternion.identity);
-            // else
-            //     Instantiate(selector, new Vector3(-2.16f, 2.348f, -0.78f), Quaternion.identity);
-
             Instantiate(item, lane_location, Quaternion.identity);
             yield return new WaitForSeconds(2f);
         }
