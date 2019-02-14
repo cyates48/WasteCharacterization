2aca5e444976e7c2f3105776177a902cacf53e26
TKingVS <tking@scu.edu>
2019-02-14 10:37:10 -0800

    Fixed spoon typo, added replay button

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,7 +8,8 @@ public class GameController : MonoBehaviour {
 
     public int totalPoints;
     public GameObject score;
-    public float time_left;
+    public float total_time;
+    private float time_left;
     public Spawner spawner;
     public ConveyorBelt conveyorBelt;
     private Text text_score;
@@ -25,6 +26,8 @@ public class GameController : MonoBehaviour {
     private Text text_streak;
     private Text text_streak_bonus;
 
+    public GameObject pointer;
+    public GameObject replayButton;
 
     enum GameState {
         Waiting,
@@ -36,20 +39,11 @@ public class GameController : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        totalPoints = 0;
-        text_score = score.GetComponent<Text>();
-        text_score.text = "0";
-        text_time = timeObj.GetComponent<Text>();
         gameState = GameState.Waiting;
-        UpdateTimerText();
-
-        streak = 0;
-        streakBonus = 0;
         text_streak = streakNumObj.GetComponent<Text>();
-        text_streak.text = "0";
+        text_time = timeObj.GetComponent<Text>();
+        text_score = score.GetComponent<Text>();
         text_streak_bonus = streakBonusObj.GetComponent<Text>();
-        text_streak_bonus.text = "";
-
     }
 
     void Update () {
@@ -75,11 +69,22 @@ public class GameController : MonoBehaviour {
     }
 
     public void StartGame() {
+        time_left = total_time;
+        totalPoints = 0;     
+        text_score.text = "0";
+        UpdateTimerText();
+        streak = 0;
+        streakBonus = 0;
+        text_streak.text = "0";
+        text_streak_bonus.text = "";
         gameBoard.SetActive(true);
         startBoard.SetActive(false);
         gameState = GameState.InProgress;
         spawner.StartSpawning();
         conveyorBelt.StartBelt();
+        pointer.SetActive(false);
+        completeText.SetActive(false);
+        replayButton.SetActive(false);
     }
 
     public void DistributePoints(bool gainPoints) {
@@ -117,6 +122,8 @@ public class GameController : MonoBehaviour {
     void EndLevel() {
     	gameState = GameState.Ended;
     	completeText.SetActive(true);
+        replayButton.SetActive(true);
         conveyorBelt.StopBelt();
+        pointer.SetActive(true);
     }
 }
